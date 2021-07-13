1f6fc3d39839b82baec6c96e7b960350045686fd
AliaksandrLeshyk <70918395+AliaksandrLeshyk@users.noreply.github.com>
2021-07-13 21:13:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+  let num = +n.toString().split('').reverse().join('');
+  return n >= 0 ? num : +Math.abs(n).toString().split('').reverse().join('');
 }
